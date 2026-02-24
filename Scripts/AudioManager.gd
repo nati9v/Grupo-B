b53d979297735e3d368e254e661619a5bf31db64
nati9v <natinatalia130@gmail.com>
2026-02-23 23:36:19 -0300

    SFX de UI

--- Scripts/AudioManager.gd
+++ Scripts/AudioManager.gd
@@ -1,17 +1,31 @@
 extends Node
 
 var music_player: AudioStreamPlayer
+var hover_player: AudioStreamPlayer
+var click_player: AudioStreamPlayer
+
 var current_music: String = ""
 
 var music_muted := false
 var sfx_muted := false
+
 
 func _ready():
 	#crea un nuevo audio stream y le asigna el bus "Musica"
 	music_player = AudioStreamPlayer.new()
 	music_player.bus = "Musica"
 	add_child(music_player)
-
+	
+	hover_player = AudioStreamPlayer.new()
+	hover_player.bus = "SFX"
+	hover_player.stream = load("res://Musica y SFX/UI-SFX/hover.mp3")
+	add_child(hover_player)
+	
+	click_player = AudioStreamPlayer.new()
+	click_player.bus = "SFX"
+	click_player.stream = load("res://Musica y SFX/UI-SFX/Click_Combo.wav")
+	add_child(click_player)
+	
 func play_music(path: String):
 	if current_music == path:
 		return # evita reiniciar la misma música
@@ -19,7 +33,13 @@ func play_music(path: String):
 	current_music = path
 	music_player.stream = load(path)
 	music_player.play()
+	
+func play_hover():
+		hover_player.play()
 
+func play_click():
+	click_player.play()
+	
 func toggle_music():
 	var bus = AudioServer.get_bus_index("Musica")
 	music_muted = !music_muted
--- Scripts/UI-niveles.gd
+++ Scripts/UI-niveles.gd
@@ -4,12 +4,35 @@ func _ready():
 	AudioManager.play_music("res://Musica y SFX/Musica/Menu-music_1.mp3")
 func _on_lvl_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI-Escenas/Cinematica.tscn")
+	AudioManager.play_click()
+
+
+	
 func _on_lvl_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Niveles/lvl2.tscn")
+	AudioManager.play_click()
 
 func _on_lvl_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Niveles/lvl3.tscn")
+	AudioManager.play_click()
 
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI-Escenas/Menu.tscn")
+	AudioManager.play_click()
+
+
+func _on_texture_button_mouse_entered() -> void:
+	AudioManager.play_hover()
+
+
+func _on_lvl_1_mouse_entered() -> void:
+	AudioManager.play_hover()
+
+
+
+func _on_lvl_2_mouse_entered() -> void:
+	AudioManager.play_hover()
+
+func _on_lvl_3_mouse_entered() -> void:
+	AudioManager.play_hover()
